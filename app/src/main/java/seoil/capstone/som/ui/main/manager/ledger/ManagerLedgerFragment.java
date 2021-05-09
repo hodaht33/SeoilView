3a258b208bc2df8b6c2857855b8f7a3207c87838
kgginam <kgginam00@naver.com>
2021-05-10 01:26:11 +0900

    add MRegit KeyBoard hide and move calendar

--- app/src/main/java/seoil/capstone/som/ui/main/manager/ledger/ManagerLedgerFragment.java
+++ app/src/main/java/seoil/capstone/som/ui/main/manager/ledger/ManagerLedgerFragment.java
@@ -1,16 +1,35 @@
 package seoil.capstone.som.ui.main.manager.ledger;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import seoil.capstone.som.R;
 
 public class ManagerLedgerFragment extends Fragment {
+
+    private MaterialCalendarView mCalendarView;
 
     public ManagerLedgerFragment() {
 
@@ -29,6 +48,111 @@ public class ManagerLedgerFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_manager_ledger, container, false);
 
+        initView(view);
+        initListener();
+
         return view;
     }
+
+    private void initView(View view) {
+
+        mCalendarView = view.findViewById(R.id.calendarViewMLedger);
+    }
+
+    private void initListener() {
+
+        mCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2021, 1, 1))
+                .setMaximumDate(CalendarDay.from(2030, 12, 32))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        mCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                new OneDayDecorator()
+        );
+
+        mCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+
+                //여기에 매출 조회 입력
+            }
+        });
+    }
+
+    class SundayDecorator implements DayViewDecorator {
+
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SundayDecorator() {
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SUNDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+
+            view.addSpan(new ForegroundColorSpan(Color.RED));
+        }
+    }
+
+    class SaturdayDecorator implements DayViewDecorator {
+
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SaturdayDecorator() {
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SATURDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+
+            view.addSpan(new ForegroundColorSpan(Color.BLUE));
+        }
+    }
+
+    class OneDayDecorator implements DayViewDecorator {
+
+        private CalendarDay date;
+
+        public OneDayDecorator() {
+            date = CalendarDay.today();
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return date != null && day.equals(date);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new StyleSpan(Typeface.BOLD));
+            view.addSpan(new RelativeSizeSpan(1.4f));
+            view.addSpan(new ForegroundColorSpan(Color.GREEN));
+            view.addSpan(new BackgroundColorSpan(Color.WHITE));
+
+        }
+
+        public void setDate(Date date) {
+
+            this.date = CalendarDay.from(date);
+        }
+    }
+
 }
--- app/src/main/java/seoil/capstone/som/ui/main/manager/statistics/ManagerStatisticsFragment.java
+++ app/src/main/java/seoil/capstone/som/ui/main/manager/statistics/ManagerStatisticsFragment.java
@@ -1,38 +1,19 @@
 package seoil.capstone.som.ui.main.manager.statistics;
 
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
-import android.text.style.BackgroundColorSpan;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.RelativeSizeSpan;
-import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.prolificinteractive.materialcalendarview.CalendarDay;
-import com.prolificinteractive.materialcalendarview.CalendarMode;
-import com.prolificinteractive.materialcalendarview.DayViewDecorator;
-import com.prolificinteractive.materialcalendarview.DayViewFacade;
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
-import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
-
-import java.time.DayOfWeek;
-import java.util.Calendar;
-import java.util.Date;
 
 import seoil.capstone.som.R;
 
 public class ManagerStatisticsFragment extends Fragment implements ManagerStatisticsContract.View {
 
-    private MaterialCalendarView mCalendarView;
+
 
     public ManagerStatisticsFragment() {
 
@@ -50,9 +31,6 @@ public class ManagerStatisticsFragment extends Fragment implements ManagerStatis
         View view = inflater.inflate(R.layout.fragment_manager_statistics, container, false);
 
 
-        initView(view);
-        initListener();
-
         return view;
     }
 
@@ -66,104 +44,7 @@ public class ManagerStatisticsFragment extends Fragment implements ManagerStatis
 
     }
 
-    private void initView(View view) {
 
-        mCalendarView = view.findViewById(R.id.calendarViewMStatistics);
-    }
 
-    private void initListener() {
 
-        mCalendarView.state().edit()
-                .setFirstDayOfWeek(Calendar.SUNDAY)
-                .setMinimumDate(CalendarDay.from(2021, 1, 1))
-                .setMaximumDate(CalendarDay.from(2030, 12, 32))
-                .setCalendarDisplayMode(CalendarMode.MONTHS)
-                .commit();
-
-        mCalendarView.addDecorators(
-                new SundayDecorator(),
-                new SaturdayDecorator(),
-                new OneDayDecorator()
-        );
-
-        mCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
-            @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                
-                //여기에 매출 조회 입력
-            }
-        });
-    }
-
-    class SundayDecorator implements DayViewDecorator {
-
-        private final Calendar calendar = Calendar.getInstance();
-
-        public SundayDecorator() {
-        }
-
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-
-            day.copyTo(calendar);
-            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
-            return weekDay == Calendar.SUNDAY;
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-
-            view.addSpan(new ForegroundColorSpan(Color.RED));
-        }
-    }
-
-    class SaturdayDecorator implements DayViewDecorator {
-
-        private final Calendar calendar = Calendar.getInstance();
-
-        public SaturdayDecorator() {
-        }
-
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-
-            day.copyTo(calendar);
-            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
-            return weekDay == Calendar.SATURDAY;
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-
-            view.addSpan(new ForegroundColorSpan(Color.BLUE));
-        }
-    }
-
-    class OneDayDecorator implements DayViewDecorator {
-
-        private CalendarDay date;
-
-        public OneDayDecorator() {
-            date = CalendarDay.today();
-        }
-
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return date != null && day.equals(date);
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            view.addSpan(new StyleSpan(Typeface.BOLD));
-            view.addSpan(new RelativeSizeSpan(1.4f));
-            view.addSpan(new ForegroundColorSpan(Color.GREEN));
-            view.addSpan(new BackgroundColorSpan(Color.WHITE));
-
-        }
-
-        public void setDate(Date date) {
-
-            this.date = CalendarDay.from(date);
-        }
-    }
 }
