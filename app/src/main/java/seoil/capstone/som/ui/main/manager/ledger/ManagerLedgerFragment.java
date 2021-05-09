b7eb9e8b0c76abd172157cedade796546b0db221
kgginam <kgginam00@naver.com>
2021-05-10 02:31:02 +0900

    added date

--- app/src/main/java/seoil/capstone/som/ui/main/manager/ledger/ManagerLedgerFragment.java
+++ app/src/main/java/seoil/capstone/som/ui/main/manager/ledger/ManagerLedgerFragment.java
@@ -11,9 +11,11 @@ import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
@@ -30,7 +32,13 @@ import seoil.capstone.som.R;
 public class ManagerLedgerFragment extends Fragment {
 
     private MaterialCalendarView mCalendarView;
+    private TextView mTextViewDate;
+    private TextView mTextViewDetailedSale;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
 
+    /*TODO:// getMonth() 사용시 값이 1 작게 리턴됨*/
     public ManagerLedgerFragment() {
 
     }
@@ -51,32 +59,54 @@ public class ManagerLedgerFragment extends Fragment {
         initView(view);
         initListener();
 
+        CalendarDay cd = mCalendarView.getSelectedDate();
+        mYear = cd.getYear();
+        mMonth = cd.getMonth() + 1;
+        mDay = cd.getDay();
+        String dateSelected = getDate(cd.getDate().toString().substring(0,3));
+        String text = "" + mYear + "년 " + mMonth + "월 " + mDay + "일 (" + dateSelected + ")";
+        mTextViewDate.setText(text);
+
         return view;
     }
 
     private void initView(View view) {
 
         mCalendarView = view.findViewById(R.id.calendarViewMLedger);
+        mTextViewDate = view.findViewById(R.id.textViewMLedgeDate);
+        mTextViewDetailedSale = view.findViewById(R.id.textViewMLedgeDetailedSale);
     }
 
     private void initListener() {
 
         mCalendarView.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
-                .setMinimumDate(CalendarDay.from(2021, 1, 1))
-                .setMaximumDate(CalendarDay.from(2030, 12, 32))
+                .setMinimumDate(CalendarDay.from(2021, 0, 1))
+                .setMaximumDate(CalendarDay.from(2030, 11, 31))
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit();
 
         mCalendarView.addDecorators(
                 new SundayDecorator(),
                 new SaturdayDecorator(),
-                new OneDayDecorator()
+                new TodayDecorator()
         );
+
+        mCalendarView.setSelectedDate(CalendarDay.today());
 
         mCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+
+
+                CalendarDay cd = mCalendarView.getSelectedDate();
+                mYear = cd.getYear();
+                mMonth = cd.getMonth() + 1;
+                mDay = cd.getDay();
+                String dateSelected = getDate(cd.getDate().toString().substring(0,3));
+                String text = "" + mYear + "년 " + mMonth + "월 " + mDay + "일 (" + dateSelected + ")";
+                mTextViewDate.setText(text);
+
 
                 //여기에 매출 조회 입력
             }
@@ -127,11 +157,11 @@ public class ManagerLedgerFragment extends Fragment {
         }
     }
 
-    class OneDayDecorator implements DayViewDecorator {
+    class TodayDecorator implements DayViewDecorator {
 
         private CalendarDay date;
 
-        public OneDayDecorator() {
+        public TodayDecorator() {
             date = CalendarDay.today();
         }
 
@@ -146,7 +176,6 @@ public class ManagerLedgerFragment extends Fragment {
             view.addSpan(new RelativeSizeSpan(1.4f));
             view.addSpan(new ForegroundColorSpan(Color.GREEN));
             view.addSpan(new BackgroundColorSpan(Color.WHITE));
-
         }
 
         public void setDate(Date date) {
@@ -155,4 +184,32 @@ public class ManagerLedgerFragment extends Fragment {
         }
     }
 
+    private String getDate(String date) {
+
+        if (date.equals("Sun")) {
+
+            return "일";
+        } else if (date.equals("Mon")) {
+
+            return "월";
+        } else if (date.equals("Tue")) {
+
+            return "화";
+        } else if (date.equals("Wed")) {
+
+            return "수";
+        } else if (date.equals("Thu")) {
+
+            return "목";
+        } else if (date.equals("Fri")) {
+
+            return "금";
+        } else if (date.equals("Sat")) {
+
+            return "토";
+        } else {
+
+            return "Error";
+        }
+    }
 }
