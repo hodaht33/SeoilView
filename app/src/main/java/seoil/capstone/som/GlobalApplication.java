f3e88fe97bcf58cd53cc125fdd9394c7f640e2cb
이성호 <tjdgh0303@gmail.com>
2021-05-24 01:15:40 +0900

    Add firebase project

--- app/src/main/java/seoil/capstone/som/GlobalApplication.java
+++ app/src/main/java/seoil/capstone/som/GlobalApplication.java
@@ -3,6 +3,7 @@ package seoil.capstone.som;
 import android.app.Application;
 import android.content.SharedPreferences;
 
+import com.google.firebase.FirebaseApp;
 import com.kakao.sdk.common.KakaoSdk;
 
 public class GlobalApplication extends Application {
@@ -12,5 +13,8 @@ public class GlobalApplication extends Application {
 
         // 카카오 어플 네이티브 앱 키
         KakaoSdk.init(this, "807b94e191e23dfd5f19907be7eec2ae");
+
+        // Firebase 어플 초기화
+        FirebaseApp.initializeApp(this);
     }
 }
