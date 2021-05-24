119a39154e8ba0239d63caa4a0c6893a60c7da54
kgginam <kgginam00@naver.com>
2021-05-24 17:10:07 +0900

    fix bug

--- app/src/main/java/seoil/capstone/som/ui/main/customer/point/CustomerPointFragment.java
+++ app/src/main/java/seoil/capstone/som/ui/main/customer/point/CustomerPointFragment.java
@@ -1,27 +1,22 @@
 package seoil.capstone.som.ui.main.customer.point;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import android.view.animation.AnimationUtils;
-import android.widget.Toast;
-
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 import seoil.capstone.som.R;
-
-import static android.widget.Toast.LENGTH_LONG;
 
 
 public class CustomerPointFragment extends Fragment implements CustomerPointContract.View, View.OnClickListener {
@@ -34,10 +29,6 @@ public class CustomerPointFragment extends Fragment implements CustomerPointCont
     private CustomerPointPresenter mPresenter;
     private String mUserId;
     private List<String> mPointDate;
-
-    private Animation mFabopen, mFabclose;
-    private boolean IsFaboepn = false;
-    private FloatingActionButton mFloatingActionSetting, mFloatingActingSave, mFloatingActingUse;
 
     public CustomerPointFragment() {
 
@@ -54,6 +45,7 @@ public class CustomerPointFragment extends Fragment implements CustomerPointCont
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
 
         View view = inflater.inflate(R.layout.fragment_customer_point, container, false);
 
@@ -73,21 +65,6 @@ public class CustomerPointFragment extends Fragment implements CustomerPointCont
         mAdapter = new CustomerPointAdapter(mPoint, getContext(), mPointDate);
         mRecyclerView.setAdapter(mAdapter);
 
-        //mRecyclerView = view.findViewById(R.id.recyclerViewPointInformation);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.setAdapter(mAdapter);
-
-        mFabopen = AnimationUtils.loadAnimation(getContext(),R.anim.fabopen);
-        mFabclose = AnimationUtils.loadAnimation(getContext(), R.anim.fabclose);
-
-        //mFloatingActionSetting = view.findViewById(R.id.floatingActionButtonSettingPoint);
-        //mFloatingActingSave = view.findViewById(R.id.floatingActionButtonSavingPoint);
-        //mFloatingActingUse = view.findViewById(R.id.floatingActionButtonUsingPoint);
-
-        mFloatingActionSetting.setOnClickListener(this);
-        mFloatingActingSave.setOnClickListener(this);
-        mFloatingActingUse.setOnClickListener(this);
-
         return view;
     }
 
@@ -105,43 +82,12 @@ public class CustomerPointFragment extends Fragment implements CustomerPointCont
 
     @Override
     public void onClick(View v) {
-        int id = v.getId();
-//        switch (id){
-//            case R.id.floatingActionButtonSettingPoint:
-//                anim();
-//                break;
-//            case R.id.floatingActionButtonSavingPoint:
-//                anim();
-//                Toast.makeText(getActivity(),"SAVING",Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.floatingActionButtonUsingPoint:
-//                anim();
-//                Toast.makeText(getActivity(),"USING",Toast.LENGTH_SHORT).show();
-//                break;
-//
-//        }
 
-    }
-    public void anim() {
-
-        if (IsFaboepn) {
-            mFloatingActingSave.startAnimation(mFabclose);
-            mFloatingActingUse.startAnimation(mFabclose);
-            mFloatingActingSave.setClickable(false);
-            mFloatingActingUse.setClickable(false);
-            IsFaboepn = false;
-        } else {
-            mFloatingActingSave.startAnimation(mFabopen);
-            mFloatingActingUse.startAnimation(mFabopen);
-            mFloatingActingSave.setClickable(true);
-            mFloatingActingUse.setClickable(true);
-            IsFaboepn = true;
-        }
     }
 
     private void initView(View view) {
 
-        //mRecyclerView = view.findViewById(R.id.recyclerViewPointInformation);
+        mRecyclerView = view.findViewById(R.id.recyclerViewCPoint);
     }
 
     @Override
