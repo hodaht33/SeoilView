278549ca99a395440d10e72978bdd76d2060a0fd
kgginam <kgginam00@naver.com>
2021-06-01 21:27:42 +0900

    fix edittext length

--- app/src/main/java/seoil/capstone/som/ui/main/manager/event/ManagerEventFragment.java
+++ app/src/main/java/seoil/capstone/som/ui/main/manager/event/ManagerEventFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
